d235f161e76741eadfa5156e46773578f169e140
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-08-31 00:26:43 +0900

    Delete hello-jpa directory

--- hello-jpa/src/main/java/org/example/Domain/Delivery/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package org.example.Domain.Delivery;
-
-public enum DeliveryStatus {
-    READY, COMP
-}
--- hello-jpa/src/main/java/org/example/Domain/Order/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package org.example.Domain.Order;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
